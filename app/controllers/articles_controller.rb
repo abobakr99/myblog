49386b3b193206317275072dcf2a3a6e6a599f9b
abobakr99 <bamangar@gmail.com>
2020-07-31 17:09:05 -0400

    fix: center form in page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,34 +1,31 @@
 class ArticlesController < ApplicationController
-   # before_action :find_article , only:[:edit,:update,:destroy]
-    before_action :new_article , only: [:new]
-    def new
-    end
+  # before_action :find_article , only:[:edit,:update,:destroy]
+  def new
+    @article = Article.new
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def create
-        @article = Article.new(article_params)
-        if @article.save
-            flash[:success] ="Article was created successfully!"
-            redirect_to root_path
-        else
-            render 'new'
-        end
-    end
+  def create
+    @article = Article.new(article_params)
+      if @article.save
+        flash[:success] ="Article was created successfully!"
+        redirect_to root_path
+      else
+        render 'new'
+      end
+  end
 
-    def edit 
-    end
+  def edit 
+  end
 
-    def update
-    end 
-    private
-    def new_article
-        @article = Article.new
-    end
+  def update
+  end 
 
-    def article_params
-        params.require(:article).permit(:title,  :text)
-    end
-
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
